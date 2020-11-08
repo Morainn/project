a08d4180deb903ab5e23652d76c3bc90ba35eee0
Morainn <74067052+Morainn@users.noreply.github.com>
2020-11-08 12:45:50 +0200

    Add files via upload

---
+++ fill_in.php
@@ -0,0 +1,170 @@
+<?php
+require_once'connection.php';
+
+	$name ="";
+	$surname ="";
+	$contact ="";
+	$date ="";
+	$age ="";
+	$food ="";
+	$eatout ="";
+	$movies ="";
+	$tv ="";
+	$radio ="";
+
+if(isset($_POST['submit_btn']))
+{
+	
+	if(empty($_POST['firstnames'])||empty($_POST['surname'])||empty($_POST['contact'])||empty($_POST['date'])||empty($_POST['age'])||empty($_POST['food'])||empty($_POST['eatout'])||
+	empty($_POST['movies'])||empty($_POST['tv'])||empty($_POST['radio1']))
+	{
+		echo 'Error, fill up all entries!!!';
+	}else{
+		
+		$name =$_POST['firstnames'];
+		$surname =$_POST['surname'];
+		$contact =$_POST['contact'];
+		$date =$_POST['date'];
+		$age =$_POST['age'];
+		$food =$_POST['food'];
+		$favfood = implode(' ',$food);
+		
+		if(!isset($_POST['eatout'])||!isset($_POST['movies'])||!isset($_POST['tv'])||!isset($_POST['radio1'])){
+			echo "All rating must be checked!!!";
+		}else{
+			$eatout =$_POST['eatout'];
+			$movies =$_POST['movies'];
+			$tv =$_POST['tv'];
+			$radio =$_POST['radio1'];
+		}
+		
+		
+		
+		$sql_insert = mysqli_query($con,"INSERT INTO details(name,surname,contact,date,age,food,eatout,movies,tv,radio)VALUES('$name','$surname','$contact','$date','$age','$favfood','$eatout','$movies','$tv','$radio')");
+	
+		if($sql_insert)
+		{
+			echo"Success!!!!!!!!!";
+			header('location:home.php'); 
+		}else{
+			echo"Fail!!!!!!!!!";
+			header('location:fill_in.php');
+		}
+	}
+	
+	
+
+}
+?>
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+<link rel="stylesheet" type="text/css" href="own_styles.css">
+<link rel="stylesheet" href="bootstrap.css">
+<style>
+</style>
+<head>
+<body>
+<header>
+<h1>Moekesti's Surveys</h1>
+</header>
+<section>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-5">	
+				<h3>Take Our Survey</h3>
+				</br>
+				<h6>Personal Details</h6>
+				<form method="post">
+					<div class="form-group">
+					<input type="text" class="form-control" id="surname" name="surname" aria-describeby="surname" placeholder="Surname">
+					<span id="surname_error" class="ch_error"></span>
+					</div>
+					<div class="form-group">
+						<input type="text" class="form-control" id="firstnames" name="firstnames" aria-describeby="firstnames" placeholder="First Names">
+						<span id="firstnames_error" class="ch_error"></span>
+					</div>
+					<div class="form-group">
+						<input type="text" class="form-control" id="contact" name="contact" placeholder="Contact Number">	
+						<span id="contact_error" class="ch_error"></span>
+					</div>
+					<div class="form-group">
+						<input type="date" class="form-control" id="date" name="date" placeholder="Date">	
+						<span id="date_error" class="ch_error"></span>
+					</div>
+					<div class="form-group">
+						<input type="text" class="form-control" id="age" name="age" aria-describeby="firstnames" placeholder="Age" min="5" max="120">
+						<span id="age_error" class="ch_error"></span>
+					</div>
+					
+					<br>
+					<div>
+					</div>
+					
+					<div class="form-group">
+						<h6>What is your favourite food?(You can choose more than one answer)</h6>
+						<p><input type="checkbox" name="food[]" value="pizza"> Pizza</p>
+						<p><input type="checkbox" name="food[]" value="pasta"> Pasta</p>
+						<p><input type="checkbox" name="food[]" value="pap"> Pap and wors</p>
+						<p><input type="checkbox" name="food[]" value="chicken"> Chicken stir fry</p>
+						<p><input type="checkbox" name="food[]" value="beef"> Beef stir fry</p>
+						<p><input type="checkbox" name="food[]" value="other"> other</p>
+						
+					</div>
+					<table id="t01">
+					<caption >On a scale of 1 to 5 indicate whether you strongly agree to strongly disagree</caption>
+					  <tr>
+						<th ></th>
+						<th nowrap>Strongly Agree(1)</th>		
+						<th>Agree(1)</th>
+						<th>Neutral(1)</th>		
+						<th>Disagree(1)</th>
+						<th nowrap>Strongly Disgree(1)</th>
+					  </tr>
+					  <tr>
+						<td nowrap>I like to eat out</td>
+						<td><input type="radio" name="eatout" value="stronglyagree"></td>
+						<td><input type="radio" name="eatout" value="agree"></td>
+						<td><input type="radio" name="eatout" value="neutral"></td>
+						<td><input type="radio" name="eatout" value="disagree"></td>
+						<td><input type="radio" name="eatout" value="stronglydisagree"> </td>
+					  </tr>
+					  <tr>
+						<td nowrap>I like to watch movies</td>
+						<td><input type="radio" name="movies" value="stronglyagree"></td>
+						<td><input type="radio" name="movies" value="agree"></td>
+						<td><input type="radio" name="movies" value="neutral"> </td>
+						<td><input type="radio" name="movies" value="disagree"></td>
+						<td><input type="radio" name="movies" value="stronglydisagree"> </td>
+					  </tr>
+					  <tr>
+						<td nowrap>I like to watch TV</td>
+						<td><input type="radio" name="tv" value="stronglyagree"></td>
+						<td><input type="radio" name="tv" value="agree"></td>
+						<td><input type="radio" name="tv" value="neutral"> </td>
+						<td><input type="radio" name="tv" value="disagree"></td>
+						<td><input type="radio" name="tv" value="stronglydisagree"> </td>
+					  </tr>
+					  <tr>
+						<td nowrap>I like to listen to the radio</td>
+						<td><input type="radio" name="radio1" value="stronglyagree"></td>
+						<td><input type="radio" name="radio1" value="agree"></td>
+						<td><input type="radio" name="radio1" value="neutral"> </td>
+						<td><input type="radio" name="radio1" value="disagree"></td>
+						<td><input type="radio" name="radio1" value="stronglydisagree"> </td>
+					  </tr>
+					</table>
+					<br>
+					<button type="submit" class="btn grey btn-block" id="submit_btn" name="submit_btn">OK</button>
+				</form>	
+			</div>
+		</div>
+	</div>
+	<br>
+	<footer>
+	Copyright © MoeketsiRaboroko.com
+	</footer>
+</section>
+<script src="bootstrap.js"></script>
+</body>
+</html>
---
+++ results.php
@@ -0,0 +1,214 @@
+<?php
+require_once'connection.php';
+?>
+<?php
+
+if(isset($_POST['ok_btn']))
+{
+	header('location:home.php'); 
+}
+
+$ages = 0;
+$oldest = 0;
+$youngest = 0;
+$totalsurveys = 0;
+$averageage = 0;
+
+$eatoutavg = 0;
+$eatavg = 0;
+$moviesavg = 0;
+$movieavg = 0;
+$tvavg = 0;
+$tavg = 0;
+$radioavg = 0;
+$radavg = 0;
+
+
+
+$query = $con->query("SELECT * FROM details") or die(mysqli_error());
+	while($fetch = $query->fetch_array()){
+		
+		$pizzapercentage = 0;
+		$pastapercentage = 0;
+		$pappercentage = 0;
+		
+		$surveys = $fetch['id'];
+		$name=$fetch['name'];
+		$surname=$fetch['surname'];
+		$contact=$fetch['contact'];
+		$date=$fetch['date'];
+		$age=$fetch['age'];
+		$food=$fetch['food'];
+		$eatout=$fetch['eatout'];
+		$movies=$fetch['movies'];
+		$tv=$fetch['tv'];
+		$radio=$fetch['radio'];
+	
+		
+		$totalsurveys = mysqli_num_rows($query);
+		$result = mysqli_query($con,"SELECT SUM(age) AS value_sum FROM details");
+		$row = mysqli_fetch_assoc($result);
+	
+		
+		for( $x=0; $x <= $totalsurveys;$x++){
+			$old = $age;
+			$young = $age;
+			
+			$ages = $row['value_sum'];
+			$averageage = $ages/$totalsurveys;
+			
+			$query = $con->query("SELECT max(age) AS max, min(age) AS min FROM details");
+			$result = mysqli_fetch_array($query);
+			
+			$oldest = $result['max'];
+			$youngest = $result['min'];
+			
+			if($old > $oldest){
+				$oldest = $old;
+			}
+			
+			if($young < $youngest){
+				$youngest = $young;
+			}
+			
+			$pizza = 0;
+			$pasta = 0;
+			$pap = 0;
+			$chicken = 0;
+			$beef = 0;
+			$other = 0;
+			
+			if($food == "pizza"){
+				$pizza++;
+			}
+			
+			if($food == "pasta"){
+				$pasta++;
+			}
+			
+			if($food == "pap"){
+				$pap++;
+			}
+			
+			if($food == "chicken"){
+				$chicken++;
+			}
+			
+			if($food == "beef"){
+				$beef++;
+			}
+			if($food == "other"){
+				$other++;
+			}
+			
+			$pizzapercentage = round($pizza / $totalsurveys * 100,1);
+			$pastapercentage = round($pasta/ $totalsurveys * 100,1);
+			$pappercentage = round($pap/ $totalsurveys * 100,1);
+			
+			
+			if($eatout == "stronglyagree"||$eatout == "agree"||$eatout == "neutral"){
+				$eatoutavg++;
+				$sql = "SELECT eatout FROM details";
+				if($result=mysqli_query($con,$sql)){
+					$rowcount=mysqli_num_rows($result);
+					$eatavg = $eatoutavg/$rowcount;
+				}
+			}else {
+				$eatoutavg--;
+			}
+			
+			if($movies == "stronglyagree"||$movies =="agree"||$movies =="neutral"){
+				$moviesavg++;
+				$sql = "SELECT movies FROM details";
+				if($result=mysqli_query($con,$sql)){
+					$rowcount=mysqli_num_rows($result);
+					$movieavg = $moviesavg/$rowcount;
+				}
+			}else{
+				$moviesavg--;
+			}
+			
+			if($tv == "stronglyagree"||$tv == "agree"||$tv == "neutral"){
+				$tvavg++;
+				$sql = "SELECT tv FROM details";
+				if($result=mysqli_query($con,$sql)){
+					$rowcount=mysqli_num_rows($result);
+					$tavg = $tvavg/$rowcount;
+				}
+			}else{
+				$tvavg--;
+			}
+			
+			if($radio== "stronglyagree"||$radio=="agree"||$radio=="neutral"){
+				$radioavg++;
+				$sql = "SELECT radio FROM details";
+				if($result=mysqli_query($con,$sql)){
+					$rowcount=mysqli_num_rows($result);
+					$radavg = $radioavg/$rowcount;
+				}
+			}else{
+				$radioavg--;
+			} 
+			
+		}
+
+	}
+?>
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+<link rel="stylesheet" type="text/css" href="own_styles.css">
+<link rel="stylesheet" href="bootstrap.css">
+<style>
+#header{
+    background-color:grey;
+    color:white;
+    text-align:center;
+    padding:5px;
+}
+</style>
+</head>
+<body>
+<header>
+<h1>Moekesti's Surveys</h1>
+</header>
+<section>
+	<div class="container left">
+		<div class="row">
+			<div class="col-sm-5">	
+				<h3>Survey Results</h3>
+				<br>
+				<form method="post">
+					<ul style="">
+						<li>Total number of surveys:    <?php echo $totalsurveys?></li><br>
+						<li>Average age:                <?php echo $averageage?></li><br>
+						<li>Oldest person who participated in the survey: <?php echo $oldest?></li><br>
+						<li>Youngest person who participated in the survey: <?php echo $youngest?></li><br>
+					
+						<br>
+					
+						<li>Percentage of people who like Pizza: <?php echo $pizzapercentage?></li><br>
+						<li>Percentage of people who like Pasta: <?php echo $pastapercentage?></li><br>
+						<li>Percentage of people who like Pap and Wors: <?php echo $pappercentage?></li><br>
+						
+						<br>
+						
+						<li>People who like to eat out: <?php echo round($eatavg,1)?><br></li>
+						<li>People who like to watch movies: <?php echo round($movieavg,1)?><br></li>
+						<li>People who like to watch TV: <?php echo round($tavg,1)?><br></li>
+						<li>People who like to listan to the radio: <?php echo round($radavg,1)?><br></li>
+					</ul>
+					<br>
+					<button type="submit" class="btn grey btn-block" id="ok_btn" name="ok_btn">OK</button>
+				</form>
+			</div>
+		</div>
+	</div>
+</section>	
+
+<br>
+<footer>
+Copyright © MoeketsiRaboroko.com
+</footer>
+</body>
+</html>
